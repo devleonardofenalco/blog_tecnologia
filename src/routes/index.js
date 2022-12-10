80b81c326f6182939607692587e242406d3770a1
Leonardo Silva Medina <leonardosilva@fenalcovalle.com>
2022-12-10 18:26:55 -0500

    Finalizacion

--- src/routes/index.js
+++ src/routes/index.js
@@ -7,18 +7,42 @@ const json_articulos = fs.readFileSync('src/articulos.json', 'utf-8');
 let articulos = JSON.parse(json_articulos);
 
 router.get('/', (req, res) => {
+  articulos = JSON.parse(fs.readFileSync('src/articulos.json', 'utf-8')); 
+     
   res.render('index', { articulos });
 }); 
 
+router.get('/admin', (req, res) => {
+  articulos = JSON.parse(json_articulos);
+  res.render('admin', { articulos });
+}); 
+  
 router.get('/leer/:id', (req, res) => {
   articulos = articulos.filter(articulo => articulo.id == req.params.id);
   articulo = articulos[0]
-  console.log(articulo) 
+  console.log(articulo)     
   res.render('articulo',{ articulo });
 
 });
+    
+router.get('/editar/:id', (req, res) => {
+  articulos = articulos.filter(articulo => articulo.id == req.params.id);
+  articulo = articulos[0]
+  console.log(articulo) 
+  res.render('editar',{ articulo });  
+  
+}); 
+
+router.get('/buscar/:palabra', (req, res) => {
+  articulos = JSON.parse(json_articulos); 
+  articulos = articulos.filter(articulo => articulo.title.toLowerCase().includes(req.params.palabra.toLowerCase()));
+  console.log("palabra :: ",req.params.palabra.toLowerCase())
+  res.render('index',{ articulos });  
+  
+});    
 
 router.get('/new-entry', (req, res) => {
+  articulos = JSON.parse(json_articulos); 
   res.render('new-entry');
 });
 
@@ -26,13 +50,25 @@ router.post('/new-entry', (req, res) => {
 
   const { title, author, image, description } = req.body;
 
+  console.log("ID :: ",req.body.id)
+  
+  id_ = uuidv4();
+  if (req.body.id!=undefined){
+    id_ = req.body.id;
+    articulos = articulos.filter(articulo => articulo.id != id_);
+    // saving data 
+    json_articulos = JSON.stringify(articulos);
+    fs.writeFileSync('src/articulos.json', json_articulos, 'utf-8');
+    
+  }
+
   if (!title || !author || !image || !description) {
     res.status(400).send("Entries must have a title and body");
     return;
   }
 
   var newarticulo = {
-    id: uuidv4(),
+    id: id_,
     title,
     author,
     image,
@@ -45,17 +81,19 @@ router.post('/new-entry', (req, res) => {
   // saving the array in a file
   const json_articulos = JSON.stringify(articulos);
   fs.writeFileSync('src/articulos.json', json_articulos, 'utf-8');
-
-  res.redirect('/');
+  
+  articulos = JSON.parse(fs.readFileSync('src/articulos.json', 'utf-8')); 
+  res.redirect('/'); 
+  
 });
 
 router.get('/delete/:id', (req, res) => {
   articulos = articulos.filter(articulo => articulo.id != req.params.id);
 
   // saving data
-  const json_articulos = JSON.stringify(articulos);
-  fs.writeFileSync('src/articulos.json', json_articulos, 'utf-8');
-
+  json_articuloss = JSON.stringify(articulos);
+  fs.writeFileSync('src/articulos.json', json_articuloss, 'utf-8');
+  
   res.redirect('/')
 });
 
